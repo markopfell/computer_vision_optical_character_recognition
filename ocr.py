31c5c9c870f2c40f8b3fd174555f8ec8f3f4ef61
Mark Opfell <markopfell@gmail.com>
2025-03-23 09:50:02 -0700

    image matching works with the character one

--- ocr.py
+++ ocr.py
@@ -1,4 +1,5 @@
 import skimage
+import numpy
 import pytest
 from matplotlib import pyplot as plt
 from skimage.feature import match_template
@@ -11,12 +12,12 @@ def read_image(image_file_name):
 
 
 def crop_template_from_image(_image):
-    # one
+    # one (double x to account for double digit numbers )
     # reading from matplotlib x and y are flipped:
     # UL (x, ) to UR (x, ) = ymin to ymax
     # UR ( ,y) to LR ( ,y) = xmin to xmax
     x_min, y_min = (690, 90)
-    x_max, y_max = (720, 115)
+    x_max, y_max = (720, 140)
 
     _template = _image[x_min:x_max, y_min:y_max]
 
@@ -27,6 +28,12 @@ image_file_name = "/Users/mark/computer_vision_optical_character_recognition/sou
 
 image = read_image(image_file_name)
 template = crop_template_from_image(image)
+result = match_template(image, template)
+# print(result)
+ij = numpy.unravel_index(numpy.argmax(result), result.shape) # referenced to (x_min, y_min)
+(x, y, d) = ij
+print(ij)
+print(x, y)
 
 
 def test_image_read():
@@ -41,4 +48,6 @@ def test_image_read():
     return
 
 def test_image_template_match():
+    plt.imshow(result, cmap='magma')
+    plt.show()
     return
