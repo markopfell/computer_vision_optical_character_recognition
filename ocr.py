e7d04030a53e3668c6dccd910a7e74e7bbd859bc
Mark Opfell <markopfell@gmail.com>
2025-03-23 09:14:44 -0700

    correct crop of image template for matching

--- ocr.py
+++ ocr.py
@@ -1,15 +1,29 @@
 import skimage
-import os.path
+import pytest
 from matplotlib import pyplot as plt
 
-image_file_name = "/Users/mark/computer_vision_optical_character_recognition/source/Sample 1_page1.jpeg"
-
-print(os.path.isfile(image_file_name))
+image_file_name = "/Users/mark/computer_vision_optical_character_recognition/source/Sample 1_page1.png"
+template_image_file_name = "/Users/mark/computer_vision_optical_character_recognition/source/one.jpeg"
 
 image = skimage.io.imread(image_file_name)
-plt.imshow(image)
-plt.show()
 
-def read_truth_image():
+#one
+# reading from matplotlib x and y are flipped:
+# UL (x, ) to UR (x, ) = ymin to ymax
+# UR ( ,y) to LR ( ,y) = xmin to xmax
+xmin, ymin = (690, 90)
+xmax, ymax = (720, 115)
+
+template_image = image[xmin:xmax, ymin:ymax]
+
+def test_image_read():
+    x, y, d = image.shape
+    print(x, y)
+    plt.imshow(template_image)
+    plt.show()
+
+    plt.imshow(image)
+    plt.show()
+
     return
 
