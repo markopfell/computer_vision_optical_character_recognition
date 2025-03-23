b11e5f8d2841783b31fc2f1a0682816f683e1a1f
Mark Opfell <markopfell@gmail.com>
2025-03-23 09:27:50 -0700

    Refactored into more functions

--- ocr.py
+++ ocr.py
@@ -1,25 +1,38 @@
 import skimage
 import pytest
 from matplotlib import pyplot as plt
+from skimage.feature import match_template
+
+
+def read_image(image_file_name):
+    image = skimage.io.imread(image_file_name)
+
+    return image
+
+
+def crop_template_from_image(_image):
+    # one
+    # reading from matplotlib x and y are flipped:
+    # UL (x, ) to UR (x, ) = ymin to ymax
+    # UR ( ,y) to LR ( ,y) = xmin to xmax
+    x_min, y_min = (690, 90)
+    x_max, y_max = (720, 115)
+
+    _template = _image[x_min:x_max, y_min:y_max]
+
+    return _template
+
 
 image_file_name = "/Users/mark/computer_vision_optical_character_recognition/source/Sample 1_page1.png"
-template_image_file_name = "/Users/mark/computer_vision_optical_character_recognition/source/one.jpeg"
 
-image = skimage.io.imread(image_file_name)
+image = read_image(image_file_name)
+template = crop_template_from_image(image)
 
-#one
-# reading from matplotlib x and y are flipped:
-# UL (x, ) to UR (x, ) = ymin to ymax
-# UR ( ,y) to LR ( ,y) = xmin to xmax
-xmin, ymin = (690, 90)
-xmax, ymax = (720, 115)
-
-template_image = image[xmin:xmax, ymin:ymax]
 
 def test_image_read():
     x, y, d = image.shape
     print(x, y)
-    plt.imshow(template_image)
+    plt.imshow(template)
     plt.show()
 
     plt.imshow(image)
@@ -27,3 +40,5 @@ def test_image_read():
 
     return
 
+def test_image_template_match():
+    return
